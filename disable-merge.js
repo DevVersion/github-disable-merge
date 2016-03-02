6387b58af1eaddfa8ef3129f5e54448bb6c8f1ec
DevVersion <paulgschwendtner@me.com>
2016-03-02 18:29:34 +0100

    feat(): disable evil button

---
+++ disable-merge.js
@@ -0,0 +1,18 @@
+(function() {
+
+	function addStyleString(str) {
+    	var node = document.createElement('style');
+    	node.innerHTML = str;
+    	document.body.appendChild(node);
+	}
+
+	addStyleString(
+		'.js-merge-branch-action {' +
+			'pointer-events: none;' +
+			'background: #FF4747;' +
+			'background-image: none;' +
+			'border-color: white;' +
+		'}'
+	);
+
+})();
